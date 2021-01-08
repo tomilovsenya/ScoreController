8bfb9cc2975b2e02e0278584d2f36d4183837ef6
Theglobalfive <54031937+Theglobalfive@users.noreply.github.com>
2021-01-08 23:07:39 +0500

    Cayo Perico, Halloween, Import Export initial support

--- Score Controller/Collections.cs
+++ Score Controller/Collections.cs
@@ -10,7 +10,10 @@ namespace Score_Controller
         public static ScoreCollection Doomsday = new ScoreCollection("The Doomsday Heist", "Doomsday", "HEIST2");
         public static ScoreCollection CasinoHeist = new ScoreCollection("Diamond Casino Heist", "Diamond Casino Heist", "HEIST3");
         public static ScoreCollection Smuggler = new ScoreCollection("Smuggler's Run", "Smuggler's Run", "SMUGGLER");
+        public static ScoreCollection Halloween = new ScoreCollection("Halloween", "Halloween", "HALLOWEEN");
         public static ScoreCollection Gunrunning = new ScoreCollection("Gunrunning", "Gunrunning", "GUNRUNNING");
+        public static ScoreCollection ImportExport = new ScoreCollection("Import Export", "Import Export", "IMPORTEXPORT");
+        public static ScoreCollection CayoPerico = new ScoreCollection("Cayo Perico Heist", "Cayo Perico Heist", "HEIST4");
         public static ScoreCollection ArenaWar = new ScoreCollection("Arena War", "Arena War", "MC_AW_MUSIC");
         public static ScoreCollection Biker = new ScoreCollection("Bikers", "Bikers", "BIKER");
         public static ScoreCollection WoodyJackson = new ScoreCollection("Woody Jackson's Tracks", "Woody Jackson", null);
@@ -23,6 +26,9 @@ namespace Score_Controller
             Assault,
             Doomsday,
             CasinoHeist,
+            CayoPerico,
+            Halloween,
+            ImportExport,
             Smuggler,
             Gunrunning,
             ArenaWar,
